4912bfdbd33a62704ecf29c90bc4ec87829a0df1
Alexey Kurylev <chepe4pi@gmail.com>
2016-11-16 12:17:55 +0300

    move connect to app ready

--- app_tx_api/apps.py
+++ app_tx_api/apps.py
@@ -1,5 +1,9 @@
-from django.apps import AppConfig
+from django.apps import AppConfig, config
+from mongoengine import connect
 
 
 class AppTxApiConfig(AppConfig):
     name = 'app_tx_api'
+
+    def ready(self):
+        connect(config.MONGO_DATABASE_NAME)
--- ethereum_scanner/__init__.py
+++ ethereum_scanner/__init__.py
@@ -1,10 +1,3 @@
-from constance import config
-from mongoengine import connect
-import django
-
 from .celery import app as celery_app
 
 __all__ = ['celery_app']
-
-django.setup()
-connect(config.MONGO_DATABASE_NAME)
